fdc0ba13f40b654790014882d9a67049e03c2b0a
tiemen <tjemen777@gmail.com>
2022-05-01 15:04:08 +0200

    jank double jump with buffer

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float runSpeed = 100f;
-    float horizontalMove=0f;
+    float horizontalMove = 0f;
 
     public int totalJumps = 2;
 
@@ -14,30 +14,75 @@ public class PlayerMovement : MonoBehaviour
     bool jump = false;
 
     bool fastFall = false;
+    public int jumpsquat = 8;
+    int shortHopBuffer = 0;
+
+    int shortHopWait = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    void Update(){
+    void Update()
+    {
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if(Input.GetButtonDown("Jump") || Input.GetAxisRaw("Vertical") == 1){
-         jump=true;
+        if (Input.GetButton("Jump") || Input.GetAxisRaw("Vertical") == 1)
+        {
+
+            shortHopBuffer++;
+
+            if (shortHopBuffer >= jumpsquat)
+            {
+                // do big jump
+                jump = true;
+                shortHopBuffer = 0;
+            }
         }
 
-        if(Input.GetAxisRaw("Vertical")==-1){
+        if (Input.GetButtonUp("Jump"))
+        {
+            if (shortHopBuffer <= jumpsquat)
+            {
+                shortHopWait = jumpsquat - shortHopBuffer;
+                // do small jump
+                jump = true;
+
+                shortHopBuffer = 0;
+            }
+        }
+
+
+
+        if (Input.GetAxisRaw("Vertical") == -1)
+        {
             fastFall = true;
+
         }
 
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime,fastFall,jump);
-        jump=false;
-        fastFall=false;
+        if (shortHopWait != 0)
+        {
+            jump = false;
+            shortHopWait--;
+
+            if (shortHopWait == 0)
+            {
+
+                controller.Move(horizontalMove * Time.fixedDeltaTime, fastFall, true);
+                jump = false;
+                fastFall = false;
+                return;
+            }
+        }
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, fastFall, jump);
+        jump = false;
+        fastFall = false;
     }
 }
